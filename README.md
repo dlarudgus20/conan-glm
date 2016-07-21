51d992f4171c99dcd5472d49360c8ef207e53881
dlarudgus20 <dlarudgus20@naver.com>
2016-07-21 14:28:18 +0900

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# glm-conan
+
+[Conan](https://conan.io) package for [GLM Library](https://github.com/g-truc/glm).
+
+The version is 0.9.7.6.
+
+The License of this package itself (not GLM Library) is BSD-2-Clause.
---
+++ conanfile.py
@@ -0,0 +1,13 @@
+from conans import ConanFile
+
+class glmConan(ConanFile):
+    name = "glm"
+    version = "0.9.7.6"
+    license = "The Happy Bunny License, or the MIT License"
+    url = "https://github.com/dlarudgus20/glm-conan"
+
+    def source(self):
+        self.run("git clone https://github.com/g-truc/glm --branch 0.9.7.6 --depth 1")
+
+    def package(self):
+        self.copy("*", dst="include/glm", src="glm/glm")
